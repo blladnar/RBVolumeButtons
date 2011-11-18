7c686e8a0e15c7218d3fb274fa3250698a683ea5
Randall <blladnar@gmail.com>
2011-11-17 20:41:35 -0500

    Initial Commit

---
+++ VolumeSnap/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  VolumeSnap
+//
+//  Created by Randall Brown on 11/17/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ VolumeSnap/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  VolumeSnap
+//
+//  Created by Randall Brown on 11/17/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
